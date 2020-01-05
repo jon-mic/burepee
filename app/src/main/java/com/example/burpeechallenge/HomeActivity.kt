6da668ebe322b56aa041c862212e9cdea9cbf0f5
jon-mic <jonasmickelger@gmail.com>
2020-01-05 17:30:59 +0100

    state 05012020

---
+++ app/src/main/java/com/example/burpeechallenge/HomeActivity.kt
@@ -0,0 +1,4 @@
+package com.example.burpeechallenge
+
+class HomeActivity {
+}
---
+++ app/src/main/java/com/example/burpeechallenge/InitActivity.kt
@@ -0,0 +1,26 @@
+package com.example.burpeechallenge
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log.d
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_init.*
+
+
+class InitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_init)
+
+        nameEnterButton.setOnClickListener {
+            var name = editName.text.toString()
+            d("jomic", "pressed by $name!")
+
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+
+    }
+
+}
